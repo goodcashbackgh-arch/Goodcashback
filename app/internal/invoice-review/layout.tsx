661bedfd8a2446a7a5f4e95106e3cd1cfd48f3c7
goodcashbackgh-arch <goodcashbackgh@gmail.com>
2026-05-02 17:29:20 +0100

    Enable compact invoice review queue UI

--- app/internal/invoice-review/layout.tsx
+++ app/internal/invoice-review/layout.tsx
@@ -1,9 +1,11 @@
 import SafeMindeeFetchPatch from "./SafeMindeeFetchPatch";
+import CompactInvoiceReviewPatch from "./CompactInvoiceReviewPatch";
 
 export default function InvoiceReviewLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SafeMindeeFetchPatch />
+      <CompactInvoiceReviewPatch />
       {children}
     </>
   );
