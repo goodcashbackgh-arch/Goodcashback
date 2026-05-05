a68fe65f7f9d207d1cc21a13bef99fade0eaa5fd
goodcashbackgh-arch <goodcashbackgh@gmail.com>
2026-05-05 10:45:06 +0100

    Mount DVA workspace used open chip enhancer

--- app/internal/dva-reconciliation/workspace/layout.tsx
+++ app/internal/dva-reconciliation/workspace/layout.tsx
@@ -1,10 +1,12 @@
 import WorkspaceAllocatedSelectionGuard from "./WorkspaceAllocatedSelectionGuard";
+import WorkspaceBalanceChipEnhancer from "./WorkspaceBalanceChipEnhancer";
 import WorkspaceSelectionEnhancer from "./WorkspaceSelectionEnhancer";
 
 export default function DvaWorkspaceLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
+      <WorkspaceBalanceChipEnhancer />
       <WorkspaceAllocatedSelectionGuard />
       <WorkspaceSelectionEnhancer />
     </>
