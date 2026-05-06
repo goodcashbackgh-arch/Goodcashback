3f7e39cf62e3f539bfaf2c076053d56c91c484c2
goodcashbackgh-arch <goodcashbackgh@gmail.com>
2026-05-06 15:23:54 +0100

    Show allocation result toast in workspace

--- app/internal/dva-reconciliation/workspace/layout.tsx
+++ app/internal/dva-reconciliation/workspace/layout.tsx
@@ -1,8 +1,10 @@
+import AllocationResultToast from "./AllocationResultToast";
 import SafeWorkspaceSelectionController from "./SafeWorkspaceSelectionController";
 
 export default function DvaWorkspaceLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <AllocationResultToast />
       {children}
       <SafeWorkspaceSelectionController />
     </>
