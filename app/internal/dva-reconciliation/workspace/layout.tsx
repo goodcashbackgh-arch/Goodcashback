0a6957506a9e6529b3ef675625efc010891fa72a
goodcashbackgh-arch <goodcashbackgh@gmail.com>
2026-05-06 13:19:35 +0100

    Disable workspace client enhancers for desktop stability

--- app/internal/dva-reconciliation/workspace/layout.tsx
+++ app/internal/dva-reconciliation/workspace/layout.tsx
@@ -1,16 +1,3 @@
-import WorkspaceAllocatedSelectionGuard from "./WorkspaceAllocatedSelectionGuard";
-import WorkspaceBalanceChipEnhancer from "./WorkspaceBalanceChipEnhancer";
-import StatementCardSelectionCompatibility from "./StatementCardSelectionCompatibility";
-import WorkspaceUrlSelectionHydrator from "./WorkspaceUrlSelectionHydrator";
-
 export default function DvaWorkspaceLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {children}
-      <WorkspaceBalanceChipEnhancer />
-      <WorkspaceAllocatedSelectionGuard />
-      <StatementCardSelectionCompatibility />
-      <WorkspaceUrlSelectionHydrator />
-    </>
-  );
+  return <>{children}</>;
 }
