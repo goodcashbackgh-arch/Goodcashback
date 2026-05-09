7a3287854c433f3853badaf5b23a7de041859aa4
goodcashbackgh-arch <goodcashbackgh@gmail.com>
2026-05-09 19:47:45 +0100

    Render resubmission action beside operator rejection notes

--- app/internal/exceptions/[dispute_id]/layout.tsx
+++ app/internal/exceptions/[dispute_id]/layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 import ExceptionStatusGuard from "./ExceptionStatusGuard";
+import RefundResubmissionNoteEnhancer from "./RefundResubmissionNoteEnhancer";
 
 export default function InternalExceptionReviewLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
+      <RefundResubmissionNoteEnhancer />
       <ExceptionStatusGuard />
     </>
   );
