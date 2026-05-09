735028471dbb405c43dbdca79121a550c4f68b15
goodcashbackgh-arch <goodcashbackgh@gmail.com>
2026-05-09 21:20:31 +0100

    Apply return evidence reviewed-row UI guard

--- app/internal/exceptions/[dispute_id]/layout.tsx
+++ app/internal/exceptions/[dispute_id]/layout.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from "react";
 import ExceptionStatusGuard from "./ExceptionStatusGuard";
 import RefundResubmissionNoteEnhancer from "./RefundResubmissionNoteEnhancer";
+import ReturnEvidenceReviewEnhancer from "./ReturnEvidenceReviewEnhancer";
 
 export default function InternalExceptionReviewLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
       <RefundResubmissionNoteEnhancer />
+      <ReturnEvidenceReviewEnhancer />
       <ExceptionStatusGuard />
     </>
   );
