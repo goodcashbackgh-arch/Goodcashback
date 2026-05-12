28dcdbb2de3051525af272c6fedeeda70230de93
goodcashbackgh-arch <goodcashbackgh@gmail.com>
2026-05-12 22:01:54 +0100

    Use server redirect for return tasks alias

--- app/shipper/return-tasks/page.tsx
+++ app/shipper/return-tasks/page.tsx
@@ -1,5 +1,23 @@
-import ReturnTasksRedirect from "./ReturnTasksRedirect";
+import { redirect } from "next/navigation";
 
-export default function ShipperReturnTasksAliasPage() {
-  return <ReturnTasksRedirect />;
+export default async function ShipperReturnTasksAliasPage({
+  searchParams,
+}: {
+  searchParams?: Promise<Record<string, string | string[] | undefined>>;
+}) {
+  const params = searchParams ? await searchParams : {};
+  const query = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(params)) {
+    if (Array.isArray(value)) {
+      for (const item of value) {
+        if (item !== undefined) query.append(key, item);
+      }
+    } else if (value !== undefined) {
+      query.set(key, value);
+    }
+  }
+
+  const queryText = query.toString();
+  redirect(`/shipper/return-actions${queryText ? `?${queryText}` : ""}`);
 }
