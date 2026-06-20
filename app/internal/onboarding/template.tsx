461a7c18b21994b96b488509e120422c6afca229
goodcashbackgh-arch <goodcashbackgh@gmail.com>
2026-06-20 09:33:22 +0100

    Avoid duplicate onboarding notice

--- app/internal/onboarding/template.tsx
+++ app/internal/onboarding/template.tsx
@@ -1,18 +1,3 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-import Notice from "./notice";
-
 export default function Template({ children }: { children: React.ReactNode }) {
-  const params = useSearchParams();
-  const message = params.get("saved");
-
-  return (
-    <>
-      <div className="mx-auto max-w-7xl px-4 pt-4 sm:px-6">
-        <Notice message={message} />
-      </div>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
